b709f7f66f89d3570b84bc3cf41944a9af80c841
unknown <camgoodwin300@gmail.com>
2021-03-07 13:50:52 -0600

    deleting chrome paths

--- hof_scraping.py
+++ hof_scraping.py
@@ -7,9 +7,8 @@ from bs4 import BeautifulSoup as soup
 import datetime as dt
 import os
 
-GOOGLE_CHROME_BIN = "/app/.apt/usr/bin/google-chrome.exe"
-CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver.exe"
-
+# GOOGLE_CHROME_BIN = "/app/.apt/usr/bin/"
+# CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver"
 chrome_options = Options()
 chrome_options.binary_location = GOOGLE_CHROME_BIN
 chrome_options.add_argument('--disable-gpu')
